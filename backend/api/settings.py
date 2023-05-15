df6f3ec944588fc2b7ddb1a4d3c9f0b0fe56c01d
Jesse Nicolas <jn@nevada.unr.edu>
2023-05-15 15:35:24 -0700

    frontend auth template

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -63,9 +63,11 @@ SITE_ID = 1
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
 
 REST_AUTH = {
     'USE_JWT': True,
