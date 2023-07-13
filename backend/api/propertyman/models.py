c2059995c0cb0e8c8fcb3cbfbc7d04b65fc6b9e4
Jesse Nicolas <jn@nevada.unr.edu>
2023-07-13 11:31:22 -0700

    extend user model

--- backend/api/propertyman/models.py
+++ backend/api/propertyman/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Manager(User):
+  phone = models.CharField(max_length=10)
+  bio = models.TextField()
+
+class Client(User):
+  address = models.CharField(max_length=100)
+  phone = models.CharField(max_length=10)
+  p_manager = models.ForeignKey(Manager, on_delete=models.CASCADE)
